03dc98d622317f0ba6416410cc8163e2e9ad9e5f
Jonatan Guzmán <jonatanguzman2712@gmail.com>
2026-03-31 20:50:23 -0300

    update fondo

---
+++ js/vanta-config.js
@@ -0,0 +1,15 @@
+let vantaEffect = VANTA.NET({
+  el: "#vanta-canvas",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3fbbff,         
+  backgroundColor: 0x090a0f, 
+  points: 10.00,           
+  maxDistance: 20.00,      
+  spacing: 15.00           
+});
